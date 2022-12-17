8280fadd394901d4c841a14d05166de30e8b0b5d
LeoAndo <leo.777.info@gmail.com>
2022-12-17 15:02:55 +0900

    edge-to-edgeを導入

--- app/src/main/java/com/leoleo/androidgithubsearch/MainActivity.kt
+++ app/src/main/java/com/leoleo/androidgithubsearch/MainActivity.kt
@@ -3,23 +3,25 @@ package com.leoleo.androidgithubsearch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.leoleo.androidgithubsearch.ui.theme.AndroidGithubSearchTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContent {
             AndroidGithubSearchTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .windowInsetsPadding(WindowInsets.safeDrawing),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
