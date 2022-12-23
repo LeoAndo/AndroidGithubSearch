05f5c232e0beb932070bd1f23a56e7e25be5c0d7
LeoAndo <leo.777.info@gmail.com>
2022-12-23 21:54:57 +0900

    add Preview Empty case for SearchScreen Composable

--- app/src/main/java/com/leoleo/androidgithubsearch/ui/search/SearchScreen.kt
+++ app/src/main/java/com/leoleo/androidgithubsearch/ui/search/SearchScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.leoleo.androidgithubsearch.R
@@ -107,7 +108,10 @@ private fun SearchScreenStateless(
                 onReload = { onSearch() },
             )
             SearchUiState.Loading -> LoadingFullScreen()
-            SearchUiState.Empty -> ErrorFullScreen(message = stringResource(id = R.string.empty_message), onReload = { onSearch() },)
+            SearchUiState.Empty -> ErrorFullScreen(
+                message = stringResource(id = R.string.empty_message),
+                onReload = { onSearch() },
+            )
             SearchUiState.Initial -> {}
 
         }
@@ -168,6 +172,19 @@ private fun Prev_Loading_SearchScreen() {
 private fun Prev_Initial_SearchScreen() {
     SearchScreenStateless(
         uiState = SearchUiState.Initial,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp),
+        query = "android-compose",
+        onValueChange = {}, onSearch = {}, onClickCardItem = { _, _ -> },
+    )
+}
+
+@PreviewPhoneDevice
+@Composable
+private fun Prev_Empty_SearchScreen() {
+    SearchScreenStateless(
+        uiState = SearchUiState.Empty,
         modifier = Modifier
             .fillMaxSize()
             .padding(12.dp),
