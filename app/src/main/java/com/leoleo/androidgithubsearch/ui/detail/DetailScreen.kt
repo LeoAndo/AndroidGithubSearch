89fef1ae2ba3d2510fafc832988ff132f5073e70
LeoAndo <leo.777.info@gmail.com>
2022-12-26 11:02:55 +0900

    delete CircleShape import

--- app/src/main/java/com/leoleo/androidgithubsearch/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/leoleo/androidgithubsearch/ui/detail/DetailScreen.kt
@@ -1,7 +1,6 @@
 package com.leoleo.androidgithubsearch.ui.detail
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
