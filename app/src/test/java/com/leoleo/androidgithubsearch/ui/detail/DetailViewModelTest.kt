26f3c6478874f2fe439e33769bbe0dfe755c5955
LeoAndo <leo.777.info@gmail.com>
2022-12-31 23:10:28 +0900

    fix: ci error

--- app/src/test/java/com/leoleo/androidgithubsearch/ui/detail/DetailViewModelTest.kt
+++ app/src/test/java/com/leoleo/androidgithubsearch/ui/detail/DetailViewModelTest.kt
@@ -1,11 +1,7 @@
 package com.leoleo.androidgithubsearch.ui.detail
 
 import com.leoleo.androidgithubsearch.MainDispatcherRule
-import com.leoleo.androidgithubsearch.data.ErrorResult
-import com.leoleo.androidgithubsearch.domain.model.RepositoryDetail
-import com.leoleo.androidgithubsearch.domain.repository.GithubRepoRepository
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.whenever
+import com.leoleo.androidgithubsearch.data.repository.FakeGithubRepoRepositoryImpl
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
@@ -15,8 +11,6 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import org.mockito.MockitoAnnotations
-import org.mockito.Spy
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(JUnit4::class)
@@ -25,24 +19,11 @@ class DetailViewModelTest {
     @get:Rule
     val mainDispatcherRule = MainDispatcherRule()
 
-    @Spy
-    private lateinit var repository: GithubRepoRepository
+    private val repository = FakeGithubRepoRepositoryImpl()
     private lateinit var viewModel: DetailViewModel
-
-    private val ownerName = "flutter"
-    private val data = RepositoryDetail(
-        name = "flutter",
-        ownerAvatarUrl = "https://avatars.githubusercontent.com/u/14101776?v=4",
-        stargazersCount = "147731",
-        forksCount = "24075",
-        openIssuesCount = "11390",
-        watchersCount = "3561",
-        language = "Dart",
-    )
 
     @Before
     fun setUp() {
-        MockitoAnnotations.initMocks(this)
         viewModel = DetailViewModel(repository)
     }
 
@@ -53,26 +34,23 @@ class DetailViewModelTest {
 
     @Test
     fun stateIsSuccess() = runTest {
+        repository.isSuccess = true
         val job1 =
             launch(mainDispatcherRule.testDispatcher) {
-                val retValue: RepositoryDetail = data
-                doReturn(retValue).whenever(repository)
-                    .getRepositoryDetail(ownerName, data.name)
-                viewModel.getRepositoryDetail(ownerName, data.name)
+                viewModel.getRepositoryDetail(repository.ownerName, repository.successData.name)
             }
-        assertEquals(UiState.Data(data), viewModel.uiState)
+        assertEquals(UiState.Data(repository.successData), viewModel.uiState)
         job1.cancel()
     }
 
     @Test
     fun stateIsError() = runTest {
+        repository.isSuccess = false
         val job1 =
             launch(mainDispatcherRule.testDispatcher) {
-                val retValue = ErrorResult.NetworkError("error!")
-                doReturn(retValue).whenever(repository).getRepositoryDetail(ownerName, data.name)
-                viewModel.getRepositoryDetail(ownerName, data.name)
+                viewModel.getRepositoryDetail(repository.ownerName, repository.successData.name)
             }
-        assert(viewModel.uiState is UiState.Error)
+        assertEquals(UiState.Error(repository.errorData), viewModel.uiState)
         job1.cancel()
     }
 }
