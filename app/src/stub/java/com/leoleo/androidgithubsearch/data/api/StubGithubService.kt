1027110a3f61f67da5434f535c0313477968306a
LeoAndo <leo.777.info@gmail.com>
2023-01-01 17:53:49 +0900

    検索APIの正常系のstubデータを追加

--- app/src/stub/java/com/leoleo/androidgithubsearch/data/api/StubGithubService.kt
+++ app/src/stub/java/com/leoleo/androidgithubsearch/data/api/StubGithubService.kt
@@ -1,16 +1,28 @@
 package com.leoleo.androidgithubsearch.data.api
 
+import android.content.Context
+import com.leoleo.androidgithubsearch.data.api.response.SearchRepositoryResponse
+import com.leoleo.androidgithubsearch.data.api.response.toModel
 import com.leoleo.androidgithubsearch.domain.model.RepositoryDetail
 import com.leoleo.androidgithubsearch.domain.model.RepositorySummary
+import com.leoleo.androidgithubsearch.test.extentions.decodeFromStubData
+import kotlinx.serialization.json.Json
 
-class StubGithubService: GithubService {
+class StubGithubService constructor(
+    private val context: Context,
+) : GithubService {
     override suspend fun searchRepositories(
         query: String,
         page: Int,
         perPage: Int,
         sort: String
     ): List<RepositorySummary> {
-        throw ApiErrorResult.UnexpectedError("stub1")
+        val format = Json { ignoreUnknownKeys = true }
+        return format.decodeFromStubData<SearchRepositoryResponse>(
+            context,
+            format,
+            "search_repositories_success.json"
+        ).toModel()
     }
 
     override suspend fun fetchRepositoryDetail(
--- app/src/stub/java/com/leoleo/androidgithubsearch/di/DataSourceModule.kt
+++ app/src/stub/java/com/leoleo/androidgithubsearch/di/DataSourceModule.kt
@@ -1,10 +1,12 @@
 package com.leoleo.androidgithubsearch.di
 
+import android.content.Context
 import com.leoleo.androidgithubsearch.data.api.GithubService
 import com.leoleo.androidgithubsearch.data.api.StubGithubService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.*
 import io.ktor.client.engine.android.*
@@ -18,5 +20,6 @@ import javax.inject.Singleton
 object DataSourceModule {
     @Singleton
     @Provides
-    fun provideGithubService(): GithubService = StubGithubService()
+    fun provideGithubService(@ApplicationContext context: Context): GithubService =
+        StubGithubService(context)
 }
